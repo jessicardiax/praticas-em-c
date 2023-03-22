13123c4480485312a581e1345e7549d95dfd24c6
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-03-22 17:17:08 -0300

    Create 1ate500.c
    Escreva um programa que exibe o somatório dos valores pares existentes na faixa
    de 1 (inclusive) até 500 (inclusive).

---
+++ 1ate500.c
@@ -0,0 +1,12 @@
+/*/Escreva um programa que exibe o somatório dos valores pares existentes na faixa
+de 1 (inclusive) até 500 (inclusive)./*/
+
+#include <stdio.h>
+int main (){
+    int cont=1;
+    printf("numeros pares de 1 ate 500:");
+    for (cont =1 ; cont <=500 ; cont++){
+        printf(" %i " ,cont);
+    }
+return(0);        
+    }

941fdafd7c27cc1db165f246e2f0b6305bde0999
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-03-22 09:58:29 -0300

    Create maior.c

---
+++ maior.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main(){
+    int N1,N2;
+    printf("digite um numero:");
+    scanf("%d",&N1);
+    printf("digite outro numero:");
+    scanf("%d",&N2);
+     if (N1>N2){
+         printf("O numero maior é:%d",N1);
+     }
+     else{
+     (N2>N1);
+     printf("O numero maior é:%d",N2);
+     }
+     return 0;
+}

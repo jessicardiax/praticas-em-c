15e13eabb3e213338f158efb022fd95b316beea5
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-03-22 09:50:29 -0300

    Create portugol.c

---
+++ portugol.c
@@ -0,0 +1,13 @@
+algoritmo "cubo"
+var
+n1, n2:inteiro
+inicio
+escreval("digite um numero")
+leia(n1)
+escreval("digite outro numero")
+leia (n2)
+n1:=n1*4
+n2:=n1*6
+escreval("O quadrado do numero �", n1)
+escreval("O cubo do numero � ", n2)
+fimalgoritmo

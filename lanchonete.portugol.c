072c8b6202676641c896459f2be8358061d1cd85
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-03-22 09:48:27 -0300

    Create lanchonete.portugol.c
    Inicialmente, o programa deve exibir as informações da tabela anterior. Em seguida,
    o programa deve solicitar e ler o código do item desejado e a quantidade de tal item.
    Por fim, o programa deve calcular e exibir o valor a ser pago. Considere que, a cada
    execução do programa, o cliente só pode escolher um item, variando apenas sua
    quantidade.

---
+++ lanchonete.portugol.c
@@ -0,0 +1,26 @@
+algoritmo "caso"
+var
+op:inteiro
+inicio
+Escreval("======informe o que voc� deseja======")
+escreval("======CARD�PIO=======")
+escreval("digite 100 para cachorro-quente")
+escreval("digite 101Bauru")
+escreval("digite 102 para X-salada")
+escreval("digite 103 para Hamburguer")
+escreval("digite 104 para Refrigerante(lata)")
+leia (op)
+escolha (op)
+caso 100
+escreval("voc� escolheu o cachorro-quente,valor R$ 7,00")
+caso 101
+escreval("voce escolheu o bauru, valor R$ 8,00")
+caso 102
+escreval("voce escolheu o X-salada, valor R$9,00")
+caso 103
+escreval("voce escolheu o X-salada, valor R$10,00")
+caso 104
+escreval("voce escolheu o refrigerante lata,valor R$ 3,00")
+
+fimescolha
+fimalgoritmo

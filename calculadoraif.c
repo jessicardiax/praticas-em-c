396020091bb99e1b74bee75d745b65e2d37356be
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-03-22 09:57:26 -0300

    Create calculadoraif.c

---
+++ calculadoraif.c
@@ -0,0 +1,19 @@
+int main()
+{
+int n1,n2,soma, divisao, multi, subtracao;
+printf("CALCULADORA 1.0\n");
+printf("digite um numero: ");
+scanf("%d", &n1);
+printf("digite um numero:");
+scanf("%d",&n2);
+ soma =n1+n2;
+ divisao=n1/n2;
+ multi=n1*n2;
+ subtracao=n1-n2;
+
+ printf("a soma é:%d/n",soma);
+ printf("a divisão é:%d/n",divisao);
+ printf("a multiplicação é: %d/n",multi);
+ printf("a subtração é %d/n",subtracao);
+ return 0;
+}

17cf1a4c970c3b12d37e24438f1d1f33fe336278
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-03-31 15:16:41 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n1,n2,soma;
+    
+    printf("Informe um numero:");
+    scanf("%d",&n1);
+    
+     printf("Informe outro numero:");
+      scanf("%d",&n2);
+      
+     soma=n1+n2;
+     printf("Resultado da soma:%d\n",soma);
+ 
+ return(0);     
+}

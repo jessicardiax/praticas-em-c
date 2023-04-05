e405757083c67e788f8585d74a6980f81d230a20
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-04-05 11:37:03 -0300

    Create test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(){
+  int a,i;
+  printf("Digite um numero inteiro");
+   scanf("%i",&i);
+  
+    for (a = 0; a < 2; a++){
+        i = i*i;
+        }
+printf("%i",i);
+    return 0;
+}

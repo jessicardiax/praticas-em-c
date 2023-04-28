61ecd20cedc6e7cd22f3e5495ca739fa842c52c4
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-04-28 14:46:51 -0300

    Create vet.c
    4. Escreva um programa que solicita e armazena dez números inteiros em um vetor e, 
    ao final, informa o maior valor encontrado no vetor

---
+++ vet.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	int i,m, vet[10];
+	for(i=0; i<=9; i++) {
+		printf("Digite o %iº valor inteiro\n", i+1);
+		scanf("%i",&vet[i]);
+	}
+     m = vet[0];  	
+  	for(i=1; i<=9; i++) { 
+	  if (vet[i] > m ){
+	  	m = vet[i];
+}
+}
+	    printf("\n**** VETOR ****\n");
+		for(i=0; i<=9; i++) {
+		printf("%i", vet[i]);
+}
+    	printf("\n>>Maior valor :%i", m);	  
+		
+	return(0);
+
+}

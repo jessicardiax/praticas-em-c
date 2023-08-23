db573623bfcee02d2ce05876005b8b00359ec55d
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-08-23 12:12:18 -0300

    Add files via upload
    Escreva um programa em linguagem C que verifique a validade de uma senha fornecida pelo usuario.
    A senha valida é o numero 1234. Devem ser impresas as seguintes mensagens:
     ACESSO PERMITIDO caso a senha seja válida
     ACESSO NEGADO caso seja inválida

--- main (27).c
+++ main (27).c
@@ -1,15 +1,20 @@
-//Desenvolva um algoritmo que receba uma hora formada por hora e minutos, e 
-//calcule a hora digitada apenas em minutos
+/*/Escreva um programa em linguagem C que verifique a validade de uma senha fornecida pelo usuario.
+A senha valida é o numero 1234. Devem ser impresas as seguintes mensagens:
+ ACESSO PERMITIDO caso a senha seja válida
+ ACESSO NEGADO caso seja inválida/*/
 
 #include <stdio.h>
-int main(){
-int hr, min;
-
-printf("Informe a hora\n");
-scanf("%i",&hr);
-
-min=hr*60;
-printf("Hora em minuto %d",min);
-
-return(0);
-}
+#include <locale.h>
+ int main(){
+     int senha;
+      printf("Olá usuario, para ter acesso a plataforma informe sua senha:");
+      scanf("%i",&senha);
+      
+      if(senha==1234){
+         printf("ACESSO PERMITIDO!");
+      }
+      else {
+          printf("ACESSO NEGADO!");
+      }
+        return(0);
+ }

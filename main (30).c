ec77d723958608524d55825c7705e338cc1a40fa
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-08-30 13:54:00 -0300

    Add files via upload
    Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
    
    O símbolo ( representa "maior que". Por exemplo:
    [0,25]  indica valores entre 0 e 25.0000, inclusive eles.
    (25,50] indica valores maiores que 25 Ex: 25.00001 até o valor 50.0000000

--- main (30).c
+++ main (30).c
@@ -1,30 +1,27 @@
-/*/Dado três valores, A, B, e C, construa um programa para verificar se estes valores 
-podem ser valores dos lados de um triângulo. Em caso positivo, verificar se o 
-triângulo é escaleno, equilátero ou isósceles./*/
 
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-float A,B,C;
-printf("Informe o valor A:");
-scanf("%f",&A);
-printf("Informe o valor B:");
-scanf("%f",&B);
-printf("Informe o valor C:");
-scanf("%f",&C);
-
-if ((A+B<C)||(B+C<A)||(A+C<B)) {
-    printf("Não é triangulo, a soma de dois lados é menor:");
-}
-return(1);
-
-if (A==B && B==C){
-    printf("Triangulo equilátero!");
-}
-if (A==B && A!=C && B!=C){
-    printf("Triangulo isoceles");
-}
- if(A!=B && B!=C && C!=A){
-     printf("Triangulo escaleno");
- }   
-return(0);
+    double x;
+    
+    printf("\nInforme um numero aleatorio!");
+    scanf("%lf",&x);
+    
+              if(x> 0 && x<=25){
+     printf("Intervalo de [0,25]%lf\n", x);
+                               }
+                               
+                  else if (x>25 && x<=50 ){
+        printf("Intervalo de [25,50]%lf\n", x);
+                                      }
+            else if (x>50 && x<=75){
+        printf("Intervalo de [50,75]%lf\n", x);
+                                }
+    else if (x>75 && x<=100){
+        printf("Intervalo de [75,100]%lf\n", x);
+    }
+    
+    else{
+        printf("FORA DO INTERVALO!");
+    }
+    return(0);
 }
